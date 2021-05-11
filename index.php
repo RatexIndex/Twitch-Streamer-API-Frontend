0329dbbd574e7fd72de3d788db36bc2c2cbe0248
RatexIndex <13403474+RatexIndex@users.noreply.github.com>
2021-05-11 12:01:50 +0200

    Added files

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>RatexIndex - Twitch</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="static/css/style.css?v01135547455555554435">
+  
+</head>
+
+<body>
+	<br>
+	<div id="Banner">Twitch Streamer List</div>
+	<br><br>
+	<script type="text/javascript" src="static/js/main.js?v015455"></script>
+	<br><br>
+	<center><div style="width:max-content;" class="alert alert-success" role="alert">
+	  <p style="font-weight: bold;">© All rights reserved 2021. Made with 💖 by <a href="https://github.com/RatexIndex" target="_blank" style="text-decoration:none; color:#3b860c; font-weight: bold;">DannyEF</a> & <a href="https://github.com/Siddhartt" target="_blank" style="text-decoration:none; color:#3b860c; font-weight: bold;">Siddhartt</a>.</p>
+	</div></center>
+	
+
+
+	<script type="text/javascript" src="static/js/main.js?v650012"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>	
+</body>
+
+</html>
